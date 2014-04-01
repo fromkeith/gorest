05204860db86b9b1695284e361c798dac3d468c4
fromkeith <keith@fromkeith.com>
2014-03-31 23:41:47 -0700

    Add the extra produces to the docs.

--- doc.go
+++ doc.go
@@ -1,7 +1,7 @@
 //
 //A RESTful style web-services framework for the Go language. </br>
 //Creating services in Go is straight forward, GoRest? takes this a step further by adding a layer that 
-//makes tedious tasks much more automated and avoids regular pitfalls. <br/> 
+//makes tedious tasks much more automated and avoids regular pitfalls. <br/>
 //This gives you the opportunity to focus more on the task at hand... minor low-level http handling.<br/>
 //
 //
@@ -14,15 +14,16 @@
 //	)
 //	func main() {
 //	    gorest.RegisterService(new(HelloService)) //Register our service
-//	    http.Handle("/",gorest.Handle())    
-// 	   http.ListenAndServe(":8787",nil)
+//	    http.Handle("/",gorest.Handle())
+// 	    http.ListenAndServe(":8787",nil)
 //	}
 //
 //	//Service Definition
 //	type HelloService struct {
-//	    gorest.RestService `root:"/tutorial/"`
+//	    gorest.RestService `root:"/tutorial/" produces:"application/json"`
 //	    helloWorld  gorest.EndPoint `method:"GET" path:"/hello-world/" output:"string"`
 //	    sayHello    gorest.EndPoint `method:"GET" path:"/hello/{name:string}" output:"string"`
+//      sayHelloXml gorest.EndPoint `method:"GET" path:"/hello/xml/{name:string}" output:"string" produces:"application/xml"`
 //	}
 //	func(serv HelloService) HelloWorld() string{
 // 	   return "Hello World"
@@ -30,4 +31,7 @@
 //	func(serv HelloService) SayHello(name string) string{
 //	    return "Hello " + name
 //	}
+//  func(serv HelloService) SayHelloXml(name string) string{
+//      return serv.SayHello(name)
+//  }
 package gorest
